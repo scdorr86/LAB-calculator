cb9a1098f6212652954ec82349e67e96545c81cf
Sean Dorris <seanc.dorris@gmail.com>
2023-01-26 20:48:44 -0600

    adjusted display to align buttons

--- main.js
+++ main.js
@@ -3,23 +3,9 @@ console.log("lab calculator!")
 //display:
 const display = document.querySelector(".display")
 
-//number buttons:
-const oneBtn = document.querySelector("#one")
-const twoBtn = document.querySelector("#two")
-const threeBtn = document.querySelector("#three")
-const fourBtn = document.querySelector("#four")
-const fiveBtn = document.querySelector("#five")
-const sixBtn = document.querySelector("#six")
-const sevenBtn = document.querySelector("#seven")
-const eightBtn = document.querySelector("#eight")
-const nineBtn = document.querySelector("#nine")
 
 //operator buttons:
 const clearBtn = document.querySelector("#clear")
-const minusBtn = document.querySelector("#minus")
-const plusBtn = document.querySelector("#plus")
-const multiplyBtn = document.querySelector("#multiply")
-const divideBtn = document.querySelector("#divide")
 const equalBtn = document.querySelector("#equal")
 const backBtn = document.querySelector("#back")
 
@@ -29,23 +15,22 @@ let equation=[]
 //oneBtn.addEventListener("click", function () {equation.push(oneBtn.innerText)})
 //twoBtn.addEventListener("click", () => {equation.push(twoBtn.innerText)})
 //threeBtn.addEventListener("click", () => {equation.push(threeBtn.innerText)})
-//fourBtn.addEventListener("click", () => {equation.push(fourBtn.innerText)})
-//fiveBtn.addEventListener("click", function () {equation.push(fiveBtn.innerText)})
-//sixBtn.addEventListener("click", () => {equation.push(sixBtn.innerText)})
-//sevenBtn.addEventListener("click", () => {equation.push(sevenBtn.innerText)})
-//eightBtn.addEventListener("click", () => {equation.push(eightBtn.innerText)})
-//nineBtn.addEventListener("click", () => {equation.push(nineBtn.innerText)})
+
 clearBtn.addEventListener("click", clearFunc)
 backBtn.addEventListener("click", () => {equation.pop();display.innerHTML = 0})
 
 const numberDisplay = (event) => {
   //document.querySelector("#disPlay").innerHTML = ""
   if (event.target.id.includes("num")) {
-    const [, nmbr] = event.target.id.split('--')
-    console.log(nmbr)
-    document.querySelector("#disPlay").innerHTML += nmbr
+    const [, nmbr] = event.target.id.split('--');
+    document.querySelector("#disPlay").innerHTML += nmbr;
+    equation.push(nmbr);
+  } else if (event.target.id.includes("op")) {
+    const [, opr] = event.target.id.split('--');
+    document.querySelector("#disPlay").innerHTML = opr;
+    equation.push(opr);
   }
-}
+ }
 
 function renderFunc() {
   console.log('hello') 
@@ -54,8 +39,6 @@ function renderFunc() {
 function clearFunc() {
   equation=[]
 }
-
-
 
 console.log(equation)
 
